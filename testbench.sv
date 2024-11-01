9f5f2b7666fb3003766287c47e5a221bb1504280
Chuts <jesusesparza242@gmail.com>
2024-10-31 18:51:27 -0600

    dsdf

--- testbench.sv
+++ testbench.sv
@@ -2,28 +2,31 @@ module testbench;
   reg [3:0] a,b,c;
   reg clk = 0;
   reg [5:0] out;
-  
-  sum sum_module (.a(a),
-                  .b(b),
-                  .c(c),
-                  .out(out)
-                 );
+  reg [2:0] count = 0;
   
   initial begin
     $dumpfile("dump.vcd"); 
     $dumpvars;
     
-    repeat (5) begin
-      #5
-      clk = !clk;
-      a = $random;
-      b = $random;
-      c = $random;
-    end
-    repeat (5) begin
-      #5
-      clk = !clk;
+    repeat (10) begin
+    	#5 clk = !clk;
     end
   end
+
+  sum sum_module (.a(a),
+                  .b(b),
+                  .c(c),
+                  .out(out)
+                 );
+ 
+  always @(clk) begin
+    if (count < 5) begin
+       a = $random;
+       b = $random;
+       c = $random;
+      count = count + 1;
+    end
+  end
+
   
 endmodule
